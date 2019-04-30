1cf629252e6ff483c0d1ee85af9edf0116c33f1b
HBKarim <find_yourlove2@yahoo.com>
2019-04-30 16:12:58 +0200

    Updates

---
+++ precache-manifest.ae81b592e5efdd3063fef89859438d04.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/royate_app_2019/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "91aac4d23b0cd01c3a93",
+    "url": "/royate_app_2019/static/css/main.70bf2486.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/royate_app_2019/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "11749645a00ae01cc4ad",
+    "url": "/royate_app_2019/static/js/1.11749645.chunk.js"
+  },
+  {
+    "revision": "26d88eac2c69090e4e58",
+    "url": "/royate_app_2019/static/js/runtime~main.26d88eac.js"
+  },
+  {
+    "revision": "91aac4d23b0cd01c3a93",
+    "url": "/royate_app_2019/static/js/main.91aac4d2.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/royate_app_2019/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/royate_app_2019/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/royate_app_2019/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "11749645a00ae01cc4ad",
+    "url": "/royate_app_2019/static/css/1.a712b75c.chunk.css"
+  },
+  {
+    "revision": "9d5790aabeeb6902939f14e273db55bb",
+    "url": "/royate_app_2019/index.html"
+  }
+];
